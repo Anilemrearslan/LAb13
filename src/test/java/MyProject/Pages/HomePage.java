aef61219604057d95a745153cdbac1b129c33005
Anilemrearslan <anilemre167@gmail.com>
2023-01-06 21:42:25 -0500

    firstcommit

--- src/test/java/MyProject/Pages/HomePage.java
+++ src/test/java/MyProject/Pages/HomePage.java
@@ -14,14 +14,6 @@ public class HomePage {
     @FindBy(xpath = "//table[@align= 'center']/tbody/tr[3]/td")
     public WebElement ManagerIDText;
 
-//    String ActualText = DriverUtil.getDriver().findElement(By.xpath("//table[@align= 'center']/tbody/tr[3]/td")).getText();
-//    String Expected = "Manger Id : mngr469688";
-//
-//        Assert.assertTrue(ActualText.contains(Expected),"Home Page Doesn't contains text as \"Manger Id : mngr469688\" , verification has Failed");
-//
-//        System.out.println("Home Page contains text as \"Manger Id : mngr469688\" , verification has passed");
-
-
     public boolean verifyManagerText(){
         return ManagerIDText.getText().equals("Manger Id : mngr469688");
     }
